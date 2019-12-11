035d35eee77d08813d0cf1fcbf3ae7201d179e3e
Caleb Willis <calebwillis50@yahoo.com>
2019-12-11 18:23:53 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -6,32 +6,16 @@ def using_unshift(array, string)
   array.unshift(string)
 end
 
-def using_unshift(array)
-
-end
-
 def using_pop(array)
   return
-end
-  
-def using_pop(array)
-  
 end
 
 def pop_with_args(array)
   return
 end
 
-def pop_with_args(array)
-
-end
-
 def using_shift(array)
   return
-end
-
-def using_shift(array)
-  
 end
 
 def shift_with_args(array)
